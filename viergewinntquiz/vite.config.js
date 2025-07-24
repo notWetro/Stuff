f50c7da3e6a243c06c1c9f27efc6d6d919c82036
wetro1 <nik.fichtner52@gmail.com>
2025-07-25 00:24:22 +0200

    added docker and docker compose

--- viergewinntquiz/vite.config.js
+++ viergewinntquiz/vite.config.js
@@ -5,7 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000,
+    watch: {
+      usePolling: true,
+    },
     host: true,
-  }
+    strictPort: true,
+    allowedHosts: ['werwoelfe.fun'],
+    port: 4000,
+    }
 })
